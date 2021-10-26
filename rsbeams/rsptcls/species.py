92e9991890ead179b33ad5dd3bc2b3f99033321f
chall <chall@radiasoft.net>
2021-10-25 21:41:05 -0600

    Refactor of species to provide a more convenient access to contiguous array and getters for existing attributes

--- rsbeams/rsptcls/species.py
+++ rsbeams/rsptcls/species.py
@@ -1,17 +1,46 @@
-from scipy.constants import c
+import numpy
 
 
 class Species:
     """
     For holding species data in rsbeams.rsdata.switchyard.Switchyard
     """
+
     def __init__(self, coordinates, charge=None, mass=None, total_charge=None):
-        self.x = coordinates[:, 0]
-        self.ux = coordinates[:, 1]
-        self.y = coordinates[:, 2]
-        self.uy = coordinates[:, 3]
-        self.ct = coordinates[:, 4]
-        self.pt = coordinates[:, 5]
+        # since coordinates is mutable this isn't really very strong protection
+        self._coordinates = coordinates
         self.charge = charge
         self.mass = mass
         self.total_charge = total_charge
+
+    @property
+    def coordinates(self):
+        return self._coordinates
+
+    @property
+    def x(self):
+        return self._coordinates[:, 0]
+
+    @property
+    def ux(self):
+        return self._coordinates[:, 1]
+
+    @property
+    def y(self):
+        return self._coordinates[:, 2]
+
+    @property
+    def uy(self):
+        return self._coordinates[:, 3]
+
+    @property
+    def ct(self):
+        return self._coordinates[:, 4]
+
+    @property
+    def pt(self):
+        return self._coordinates[:, 5]
+
+    @property
+    def macroparticle_count(self):
+        return self._coordinates.shape[0]
