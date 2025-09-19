a84d087f5429798f68afde3f68f90559af8105de
Rob Nagler <5495179+robnagler@users.noreply.github.com>
2025-09-19 14:10:30 +0000

    Fix #53 upgrade to importlib.metadata

--- rsbeams/__init__.py
+++ rsbeams/__init__.py
@@ -3,10 +3,11 @@
 :copyright: Copyright (c) 2024 RadiaSoft LLC.  All Rights Reserved.
 :license: https://www.apache.org/licenses/LICENSE-2.0.html
 """
-import pkg_resources
+
+import importlib.metadata
 
 try:
+    __version__ = importlib.metadata.version("rsbeams")
+except importlib.metadata.PackageNotFoundError:
     # We only have a version once the package is installed.
-    __version__ = pkg_resources.get_distribution("rsbeams").version
-except pkg_resources.DistributionNotFound:
     pass
